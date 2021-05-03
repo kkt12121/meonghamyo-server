98c5b4d565b8819f19d1feedac1e61d9c7f9a9ea
kkt12121 <kkt34343@gmail.com>
2021-05-03 13:34:06 +0900

    [server]feat/comment5

--- controller/content/parceloutpage.js
+++ controller/content/parceloutpage.js
@@ -1,11 +1,20 @@
 const { content } = require('../../models');
+const { user } = require('../../models');
 
 module.exports = {
     get: async (req, res) => {
-        // boardName이 parcelOutContent에 해당하는 content만 불러온다                
-           const contentInfo = await content.findAll({
-               where: { boardName: "parcelOutContent" }
+        // boardName이 parcelOutContent에 해당하는 content만 불러온다
+        // contentInfo 안에 작성한 유저의 닉네임을 포함시킨다       
+           const contentInfo = await content.findAll({               
+                where: {
+                    boardName: "parcelOutContent"
+                },
+                include: [{
+                    model: user,                  
+                    attributes: ['nickname'],
+                }]
            })
+        //    console.log(userNickName)
            res.status(200).json({ data: [{contentInfo}], message: ' sell page ok' })          
        }  
 }
