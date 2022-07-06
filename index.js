f6d1adf36b7d2d9b0607a2832e2d1143c0ea5b96
kkt12121 <kkt34343@gmail.com>
2022-07-07 00:38:07 +0900

    heroku_deploy

--- index.js
+++ index.js
@@ -15,11 +15,18 @@ const userRouter = require("./routes/user");
 const mypageRouter = require("./routes/mypage");
 const contentRouter = require("./routes/content");
 
-const sequelize = require("./models").sequelize;
+const { sequelize } = require("./models");
 
 const app = express();
 
-sequelize.sync();
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log("데이터베이스 연결 성공");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 // 배포
 const options = {
