0d1312567fddc71f7df8067d1053210a7a9f502b
kkt12121 <kkt34343@gmail.com>
2022-07-12 00:30:55 +0900

    heroku_deploy

--- controller/user/login.js
+++ controller/user/login.js
@@ -1,22 +1,22 @@
-const { user } = require('../../models');
+const { user } = require("../../models");
 
 module.exports = {
-    post: async (req, res) => {
-    
+  post: async (req, res) => {
     // db에 일치하는 email과 password가 존재하는지 찾는다
     const userInfo = await user.findOne({
-        where: { email: req.body.email, password: req.body.password }
-    })
-    
+      where: { email: req.body.email, password: req.body.password },
+    });
+    console.log("이게 로그인한 유저 정보야 !!!!!!!", userInfo);
     // userInfo가 없으면 401오류를 메세지와 보낸다
-    if(!userInfo) {
-        return res.status(401).send({ message: "wrong login request" })
+    if (!userInfo) {
+      return res.status(401).send({ message: "wrong login request" });
     } else {
-        // 존재할경우 session에 userId에 유저의 id를 저장하고 유저 정보를 데이터로 보낸다
-        req.session.save(() => {
-            req.session.userId = userInfo.id
-            res.status(200).send({ data: [{userInfo}], message: 'ok' })
-        })       
+      // 존재할경우 session에 userId에 유저의 id를 저장하고 유저 정보를 데이터로 보낸다
+      req.session.save(() => {
+        console.log("이게 로그인한 유저 id야 !!!!!!!", userInfo.id);
+        req.session.userId = userInfo.id;
+        res.status(200).send({ data: [{ userInfo }], message: "ok" });
+      });
     }
-  }
-}
+  },
+};
