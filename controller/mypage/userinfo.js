bf8473f428a475eb461442da2d94789c9585f532
kkt12121 <kkt34343@gmail.com>
2022-07-12 18:48:41 +0900

    heroku_deploy

--- controller/mypage/userinfo.js
+++ controller/mypage/userinfo.js
@@ -3,6 +3,7 @@ const { user } = require("../../models");
 module.exports = {
   get: async (req, res) => {
     console.log("세션 정보가 들어있네??????????????????????", req.session);
+    console.log("뭐야 있었자나?????????????", req.session.userId);
     // session에 있는 userId가 없으면 없다는 메세지를 보낸다
     if (!req.session.userId) {
       return res.status(401).json({ message: "not authorized" });
